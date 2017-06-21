e11b828cd217bbd6679061c2b5b7bdb54729784d
TitanSnow <sweeto@live.cn>
2017-06-21 13:06:17 +0800

    add type hints to storage

--- actiontools/storage.py
+++ actiontools/storage.py
@@ -2,11 +2,13 @@ import shelve
 from getpass import getuser
 from os import getpid, path
 
-def open_local():
+Shelf = shelve.Shelf
+
+def open_local() -> Shelf:
     return shelve.open(".actiontools_local_storage_" + getuser())
 
-def open_session():
+def open_session() -> Shelf:
     return shelve.open(".actiontools_session_storage_" + str(getpid()))
 
-def open_global():
+def open_global() -> Shelf:
     return shelve.open(path.join(path.expanduser("~"), ".actiontools_global_storage"))
