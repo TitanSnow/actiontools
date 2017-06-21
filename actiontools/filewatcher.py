0c1f7fab1b756269faf88996008c94f9bc562e97
TitanSnow <sweeto@live.cn>
2017-06-21 15:01:15 +0800

    add `abs` API for filewatcher

--- actiontools/filewatcher.py
+++ actiontools/filewatcher.py
@@ -19,10 +19,43 @@ def peek_is_updated(pathname: str, newfid: Optional[Fileid] = None) -> bool:
     return oldfid != newfid
 
 def get_is_updated(pathname: str, newfid: Optional[Fileid] = None) -> bool:
+    """
+    get is updated of a given path
+    this will update stored fileid
+    """
     if newfid is None:
         newfid = get_fileid(pathname)
     is_updated = peek_is_updated(pathname, newfid)
     if is_updated:
         # update store
         set_stored_fileid(newfid)
+    return is_updated
+
+def get_abs_stored_fileid(pathname: str) -> Optional[Fileid]:
+    with open_global() as gs:
+        return gs.get(pathname)
+
+def set_abs_stored_fileid(fid: Fileid) -> None:
+    with open_global() as gs:
+        gs[fid[0]] = fid
+
+def peek_abs_is_updated(pathname: str, newfid: Optional[Fileid] = None) -> bool:
+    oldfid = get_abs_stored_fileid(pathname)
+    if oldfid is None:
+        return True
+    if newfid is None:
+        newfid = get_abs_fileid(pathname)
+    return oldfid != newfid
+
+def get_abs_is_updated(pathname: str, newfid: Optional[Fileid] = None) -> bool:
+    """
+    get is updated of a given abspath
+    this will update stored fileid
+    """
+    if newfid is None:
+        newfid = get_abs_fileid(pathname)
+    is_updated = peek_abs_is_updated(pathname, newfid)
+    if is_updated:
+        # update store
+        set_abs_stored_fileid(newfid)
     return is_updated
