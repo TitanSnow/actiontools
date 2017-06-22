e1631c994c56714b5dcf68957a725e14395dee28
TitanSnow <sweeto@live.cn>
2017-06-22 12:44:24 +0800

    print command before action in `vdo_shell`

--- actiontools/callexternal.py
+++ actiontools/callexternal.py
@@ -2,6 +2,7 @@ import subprocess
 from typing import Iterable
 
 def vdo_shell(cmd: str) -> None:
+    print(cmd)
     subprocess.check_call(cmd, shell = True)
 
 def vdo_shells(cmds: Iterable[str]) -> None:
