320eb87d628fba27ab83e7cbe83dc7df45d0d476
TitanSnow <sweeto@live.cn>
2017-06-22 12:45:40 +0800

    add `setup.py`

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name = "actiontools",
+    version = "0.0.0.dev0",
+    packages = ["actiontools"],
+    author = "TitanSnow",
+    author_email = "tttnns1024@gmail.com",
+    url = "https://github.com/TitanSnow/actiontools",
+)
