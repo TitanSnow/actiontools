ed7ab352a480e9bdeaab32097c62d71f1c3f4330
TitanSnow <sweeto@live.cn>
2017-06-24 21:29:57 +0800

    improved `callexternal`
    add flag support

--- actiontools/callexternal.py
+++ actiontools/callexternal.py
@@ -1,28 +1,36 @@
 import subprocess
-from typing import Iterable
+from typing import Iterable, Optional, Sequence
 from .storage import get_storage, join_storage_path
 
-def vdo_shell(cmd: str) -> None:
-    print(cmd)
-    subprocess.check_call(cmd, shell = True)
+def do_shell(cmd: str, flag: str = '') -> Optional[subprocess.CalledProcessError]:
+    """
+    do `cmd`
+    `flag` is a str, the combination of log-control flag and error-ignore flag
+    log-control flag can be one of 'v' (verbose), 'q' (quiet)
+    when not specified, is controlled by whether storage 'actiontools.verbose' is True
+    error-ignore flag is 'i'
+    when specified, will return exception object instead of raise it
+    when there raising subprocess.CalledProcessError on cmd starts with '-'
+    """
+    ignore = 'i' in flag and cmd.startswith('-')
+    if ignore:
+        cmd = cmd[1:]
+    try:
+        if 'v' in flag or ('q' not in flag and get_storage(join_storage_path(['actiontools', 'verbose'])) is True):
+            print(cmd)
+            subprocess.check_call(cmd, shell=True)
+        else:
+            subprocess.check_call(cmd, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    except subprocess.CalledProcessError as e:
+        if not ignore:
+            raise e
+        else:
+            return e
 
-def vdo_shells(cmds: Iterable[str]) -> None:
-    for cmd in cmds:
-        vdo_shell(cmd)
-
-def qdo_shell(cmd: str) -> None:
-    subprocess.check_call(cmd, shell = True, stdout = subprocess.DEVNULL, stderr = subprocess.DEVNULL)
-
-def qdo_shells(cmds: Iterable[str]) -> None:
-    for cmd in cmds:
-        qdo_shell(cmd)
-
-def do_shell(cmd: str) -> None:
-    if get_storage(join_storage_path(['actiontools', 'verbose'])) is True:
-        vdo_shell(cmd)
-    else:
-        qdo_shell(cmd)
-
-def do_shells(cmds: Iterable[str]) -> None:
-    for cmd in cmds:
-        do_shell(cmd)
+def do_shells(cmds: Iterable[str], flag: str = '') -> Sequence[Optional[subprocess.CalledProcessError]]:
+    """
+    do `cmds`.
+    returns a list of return value returned by `do_shell`
+    `flag` applies to all cmds, has same meaning as `do_shell`
+    """
+    return [do_shell(cmd, flag) for cmd in cmds]
