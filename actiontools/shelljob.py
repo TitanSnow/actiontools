a02af02666aa8aacce7f384d350f4dc3701af5d2
TitanSnow <sweeto@live.cn>
2017-06-24 21:35:18 +0800

    add comment

--- actiontools/shelljob.py
+++ actiontools/shelljob.py
@@ -2,6 +2,7 @@ from .job import *
 from .callexternal import do_shells
 
 class ShellJob(Job):
+    """class ShellJob"""
     shells_before = []
     shells_on = []
     shells_after = []
@@ -9,8 +10,11 @@ class ShellJob(Job):
     shells_flag_on = ''
     shells_flag_after = ''
     def before(self) -> None:
+        """do `self.shells_before` with flag `self.shells_flag_before`"""
         do_shells(self.shells_before, self.shells_flag_before)
     def on(self) -> None:
+        """do `self.shells_on` with flag `self.shells_flag_on`"""
         do_shells(self.shells_on, self.shells_flag_on)
     def after(self) -> None:
+        """do `self.shells_after` with flag `self.shells_flag_after`"""
         do_shells(self.shells_after, self.shells_flag_after)
