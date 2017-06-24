7d778f05bfb982375c77a9b10887e254b06f8bd1
TitanSnow <sweeto@live.cn>
2017-06-24 19:57:29 +0800

    add qdo & do for `do_shell`

--- actiontools/callexternal.py
+++ actiontools/callexternal.py
@@ -1,5 +1,6 @@
 import subprocess
 from typing import Iterable
+from .storage import get_storage, join_storage_path
 
 def vdo_shell(cmd: str) -> None:
     print(cmd)
@@ -8,3 +9,20 @@ def vdo_shell(cmd: str) -> None:
 def vdo_shells(cmds: Iterable[str]) -> None:
     for cmd in cmds:
         vdo_shell(cmd)
+
+def qdo_shell(cmd: str) -> None:
+    subprocess.check_call(cmd, shell = True, stdout = subprocess.DEVNULL, stderr = subprocess.DEVNULL)
+
+def qdo_shells(cmds: Iterable[str]) -> None:
+    for cmd in cmds:
+        qdo_shell(cmd)
+
+def do_shell(cmd: str) -> None:
+    if get_storage(join_storage_path(['actiontools', 'verbose'])) is True:
+        vdo_shell(cmd)
+    else:
+        qdo_shell(cmd)
+
+def do_shells(cmds: Iterable[str]) -> None:
+    for cmd in cmds:
+        do_shell(cmd)
