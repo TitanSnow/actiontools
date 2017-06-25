cfa2a579338b4b371b31ca9b143c3fe679a95471
TitanSnow <sweeto@live.cn>
2017-06-25 20:41:58 +0800

    add `lisp`

---
+++ actiontools/lisp.py
@@ -0,0 +1,28 @@
+# Copyright (c) 2017 TitanSnow; All Rights Reserved
+
+
+from typing import Iterable, Callable, Any
+
+class LispMachine:
+    """class LispMachine"""
+    functable = {}
+    def eval(self, lst: Iterable) -> Any:
+        """eval `lst`"""
+        def _isevalable(lst: Any) -> bool:
+            return isinstance(lst, Iterable)
+        def _resolve_func(symbol: Any) -> Callable:
+            if isinstance(symbol, Callable):
+                return symbol
+            if isinstance(symbol, str):
+                return self.functable[symbol]
+            if _isevalable(symbol):
+                return _resolve_func(_eval(symbol))
+        def _eval(lst: Iterable) -> Any:
+            it = iter(lst)
+            func = _resolve_func(next(it))
+            def get_args() -> Iterable:
+                while True:
+                    item = next(it)
+                    yield _eval(item) if _isevalable(item) else item
+            return func(*get_args())
+        return _eval(lst)
