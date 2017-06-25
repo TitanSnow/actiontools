9eb7ddb2fe4fc406ef96b6b3d772196e0b67279d
TitanSnow <sweeto@live.cn>
2017-06-25 11:02:58 +0800

    `callexternal.call&calls`

--- actiontools/callexternal.py
+++ actiontools/callexternal.py
@@ -2,38 +2,41 @@
 
 
 import subprocess
-from typing import Iterable, Optional, Sequence
+from typing import Iterable, Optional, Sequence, Union
 from .storage import get_storage, join_storage_path
 
-def do_shell(cmd: str, flag: str = '') -> Optional[subprocess.CalledProcessError]:
+def call(cmd: Union[str, Sequence[str]], flag: str = '') -> Optional[subprocess.CalledProcessError]:
     """
     do `cmd`
-    `flag` is a str, the combination of log-control flag and error-ignore flag
+    `flag` is a str, the combination of log-control flag, error-ignore flag and shell flag
     log-control flag can be one of 'v' (verbose), 'q' (quiet)
     when not specified, is controlled by whether storage 'actiontools.verbose' is True
     error-ignore flag is 'i'
     when specified, will return exception object instead of raise it
     when there raising subprocess.CalledProcessError on cmd starts with '-'
+    shell flag is 's'
+    when specified, cmd will be parsed as a shell string
     """
-    ignore = 'i' in flag and cmd.startswith('-')
+    ignore = 'i' in flag and len(cmd) and cmd[0] == '-'
+    shell = 's' in flag
     if ignore:
         cmd = cmd[1:]
     try:
         if 'v' in flag or ('q' not in flag and get_storage(join_storage_path(['actiontools', 'verbose'])) is True):
             print(cmd)
-            subprocess.check_call(cmd, shell=True)
+            subprocess.check_call(cmd, shell = shell)
         else:
-            subprocess.check_call(cmd, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+            subprocess.check_call(cmd, shell = shell, stdout = subprocess.DEVNULL, stderr = subprocess.DEVNULL)
     except subprocess.CalledProcessError as e:
         if not ignore:
             raise e
         else:
             return e
 
-def do_shells(cmds: Iterable[str], flag: str = '') -> Sequence[Optional[subprocess.CalledProcessError]]:
+def calls(cmds: Iterable[Union[str, Sequence[str]]], flag: str = '') -> Sequence[Optional[subprocess.CalledProcessError]]:
     """
     do `cmds`.
-    returns a list of return value returned by `do_shell`
-    `flag` applies to all cmds, has same meaning as `do_shell`
+    returns a list of return value returned by `call`
+    `flag` applies to all cmds, has same meaning as `call`
     """
-    return [do_shell(cmd, flag) for cmd in cmds]
+    return [call(cmd, flag) for cmd in cmds]
--- actiontools/shelljob.py
+++ actiontools/shelljob.py
@@ -2,7 +2,7 @@
 
 
 from .job import *
-from .callexternal import do_shells
+from .callexternal import calls
 
 class ShellJob(Job):
     """class ShellJob"""
@@ -14,10 +14,10 @@ class ShellJob(Job):
     shells_flag_after = ''
     def before(self) -> None:
         """do `self.shells_before` with flag `self.shells_flag_before`"""
-        do_shells(self.shells_before, self.shells_flag_before)
+        calls(self.shells_before, self.shells_flag_before)
     def on(self) -> None:
         """do `self.shells_on` with flag `self.shells_flag_on`"""
-        do_shells(self.shells_on, self.shells_flag_on)
+        calls(self.shells_on, self.shells_flag_on)
     def after(self) -> None:
         """do `self.shells_after` with flag `self.shells_flag_after`"""
-        do_shells(self.shells_after, self.shells_flag_after)
+        calls(self.shells_after, self.shells_flag_after)
