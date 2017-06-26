633f5302e0b80b32053057888fa099c5d5c4419f
TitanSnow <sweeto@live.cn>
2017-06-26 08:01:48 +0800

    new `callexternal`

--- actiontools/callexternal.py
+++ actiontools/callexternal.py
@@ -2,45 +2,55 @@
 
 
 import subprocess
-from typing import Iterable, Optional, Sequence, Union
-from .storage import get_storage, join_storage_path
+from typing import Callable
+from .storage import *
 
-class ctr(str):
-    """cmd control node type"""
-    pass
+def call(*cmd) -> None:
+    if get_storage(join_storage_path(['actiontools', 'verbose'])) is True:
+        print(cmd)
+        subprocess.check_call(cmd)
+    else:
+        subprocess.check_call(cmd, stdout = subprocess.DEVNULL, stderr = subprocess.DEVNULL)
 
-def call(cmd: Union[str, Sequence[Union[ctr, str]]], flag: str = '') -> Optional[Union[subprocess.SubprocessError, OSError]]:
-    """
-    do `cmd`
-    `flag` is a str, the combination of log-control flag, error-ignore flag and shell flag
-    log-control flag can be one of 'v' (verbose), 'q' (quiet)
-    when not specified, is controlled by whether storage 'actiontools.verbose' is True
-    error-ignore flag is 'i'
-    when specified, will return exception object instead of raise it
-    when there raising subprocess.SubprocessError or OSError on cmd starts with ctr '-'
-    shell flag is 's'
-    when specified, cmd will be parsed as a shell string
-    """
-    ignore = 'i' in flag and len(cmd) and isinstance(cmd[0], ctr) and cmd[0] == '-'
-    shell = 's' in flag
-    if ignore:
-        cmd = cmd[1:]
-    try:
-        if 'v' in flag or ('q' not in flag and get_storage(join_storage_path(['actiontools', 'verbose'])) is True):
-            print(cmd)
-            subprocess.check_call(cmd, shell = shell)
-        else:
-            subprocess.check_call(cmd, shell = shell, stdout = subprocess.DEVNULL, stderr = subprocess.DEVNULL)
-    except (subprocess.SubprocessError, OSError) as e:
-        if not ignore:
-            raise e
-        else:
-            return e
+def shell(cmd: str) -> None:
+    if get_storage(join_storage_path(['actiontools', 'verbose'])) is True:
+        print(cmd)
+        subprocess.check_call(cmd, shell = True)
+    else:
+        subprocess.check_call(cmd, shell = True, stdout = subprocess.DEVNULL, stderr = subprocess.DEVNULL)
 
-def calls(cmds: Iterable[Union[str, Sequence[Union[ctr, str]]]], flag: str = '') -> Sequence[Optional[Union[subprocess.SubprocessError, OSError]]]:
-    """
-    do `cmds`.
-    returns a list of return value returned by `call`
-    `flag` applies to all cmds, has same meaning as `call`
-    """
-    return [call(cmd, flag) for cmd in cmds]
+def verbose(func: Callable, *args) -> Any:
+    ph = join_storage_path(['actiontools', 'verbose'])
+    with open_session() as db:
+        try:
+            origin = db[ph]
+            db[ph] = True
+            try:
+                rv = func(*args)
+            finally:
+                db[ph] = origin
+        except KeyError:
+            db[ph] = True
+            try:
+                rv = func(*args)
+            finally:
+                del db[ph]
+    return rv
+
+def quiet(func: Callable, *args) -> Any:
+    ph = join_storage_path(['actiontools', 'verbose'])
+    with open_session() as db:
+        try:
+            origin = db[ph]
+            db[ph] = False
+            try:
+                rv = func(*args)
+            finally:
+                db[ph] = origin
+        except KeyError:
+            db[ph] = False
+            try:
+                rv = func(*args)
+            finally:
+                del db[ph]
+    return rv
