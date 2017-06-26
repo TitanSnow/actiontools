396232f42b80899188bfb8dc657c7389005cf77e
TitanSnow <sweeto@live.cn>
2017-06-26 11:42:12 +0800

    get default_value from storage in `do_once`

--- actiontools/job.py
+++ actiontools/job.py
@@ -4,7 +4,8 @@
 from threading import Thread
 from queue import Queue, Empty
 from time import sleep
-from typing import Collection
+from typing import Collection, Optional
+from .storage import get_storage, join_storage_path
 
 class Job:
     """class Job"""
@@ -25,11 +26,15 @@ class TemporarilyNotAvailable(RuntimeError):
     def __init__(self, err_msg: str = "Temporarily not available. Retry needed") -> None:
         super().__init__(err_msg)
 
-def do_once(joblist: Collection[Job], maxjobs: int = 1, idle_wait_sleeptime: float = 0.01):
+def do_once(joblist: Collection[Job], maxjobs: Optional[int] = None, idle_wait_sleeptime: Optional[float] = None):
     """
     do `maxjobs` of jobs at once
     if maxjobs <= 0, it will be set to len(joblist)
     """
+    if maxjobs is None:
+        maxjobs = get_storage(join_storage_path([__name__, 'do_once', 'maxjobs']), 1)
+    if idle_wait_sleeptime is None:
+        idle_wait_sleeptime = get_storage(join_storage_path([__name__, 'do_once', 'idle_wait_sleeptime']), 0.01)
     if maxjobs <= 0:
         maxjobs = len(joblist)
     q = Queue()
